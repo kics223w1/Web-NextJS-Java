b0e1faeb5c8471dbf6795483debfd72b7d5414c8
Cao Viết Huy <hieppro138@gmail.com>
2023-12-26 19:30:51 +0700

    init

---
+++ components/main-page/header-main-page.tsx
@@ -0,0 +1,32 @@
+import { Text } from "@chakra-ui/react";
+import JordanIcon from "../../icons/jordan-icon";
+
+const HeaderMainPage = () => {
+  return (
+    <div className="h-9 w-full bg-[#F5F5F5] px-12 flex items-center justify-between">
+      <JordanIcon className="cursor-pointer" onClick={() => {}} />
+      <div className="flex items-center gap-3 h-full">
+        <Text className="font-medium text-xs hover:text-[#747474] cursor-pointer">
+          Find a Store
+        </Text>
+        <div className="w-[1px] bg-black h-3" />
+
+        <Text className="font-medium text-xs hover:text-[#747474] cursor-pointer">
+          Help
+        </Text>
+        <div className="w-[1px] bg-black h-3" />
+
+        <Text className="font-medium text-xs hover:text-[#747474] cursor-pointer">
+          Join Us
+        </Text>
+        <div className="w-[1px] bg-black h-3" />
+
+        <Text className="font-medium text-xs hover:text-[#747474] cursor-pointer">
+          Sign In
+        </Text>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderMainPage;
---
+++ components/main-page/main-page.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import JordanIcon from "../../icons/jordan-icon";
+import HeaderMainPage from "./header-main-page";
+
+const MainPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Nike. Just Do It. Nike VN</title>
+      </Head>
+
+      <HeaderMainPage />
+    </>
+  );
+};
+
+export default MainPage;
